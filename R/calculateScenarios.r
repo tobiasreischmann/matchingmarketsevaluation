6262096222e38603796e9ee0d3c044a665b006d8
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2019-10-31 09:57:27 +0100

    Created project and added three vignettes

---
+++ R/calculateScenarios.r
@@ -0,0 +1,137 @@
+
+# ----------------------------------------------------------------------------
+# R-code for analyzing the rounds played in decentralized college admission problem
+# of the matchingmarkets package.
+# For multiple dimensions it is analysed how a dimension influences the output for multiple
+# exemplary scenarios.
+#
+# Copyright (c) 2019 Tobias Reischmann
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file LICENSE
+#
+# ----------------------------------------------------------------------------
+
+#' @title Simulate multiple scenarios for the college admissions problem
+#'
+#' @param scenarios list of lists containing the different scenarios.
+#' @param nruns integer indicating the number of markets to be simulated (results are averaged over all simulated markets).
+#' @param nworkers integer number of workers generated for the parallel package.
+#'
+#' @export
+#'
+#' @return
+#'
+#' @return
+#' \code{calculateScenarios} returns a list of lists, which contains the following fields
+#' \item{occupancyrate}{double indicating the ratio of #students/#availableplaces}
+#' \item{nStudents}{integer indicating the number of students per market}
+#' \item{nColleges}{integer indicating the number of colleges per market}
+#' \item{threshold}{double influencing the number of decentrailzed rounds played. The mechanism terminates if the ratio of places, which are different in comparison to the finished mechanism are below this percentage value.}
+#' \item{areasize}{integer indicating the length of the grid used for the horizontal preferences.}
+#' \item{horizontalscenario}{integer (0,1,2) indicating which colleges uses horizontal preferences in their ranking (1=>all, 2=>only public colleges, 3=> none).}
+#' \item{conf.s.prefs}{vector representing the size of the tiers for students' ranking lists}
+#' \item{quota}{double between 0 and 1 indicating the percentage of private facilities}
+#'
+#' @author Tobias Reischmann
+#'
+#' @keywords generate
+#'
+#' @examples
+#'
+#' ## Simulate a set of different scenarios and return the average number of decentralized rounds played.
+#'
+#' elem1 <- list(occupancyrate = .8, quota = .3, nStudents = 2700, nColleges = 600,
+#'               areasize = 7, conf.s.prefs = c(3,7,10,10), horizontalscenario = 1)
+#' elem2 <- list(occupancyrate = .8, quota = .3, nStudents = 600, nColleges = 200,
+#'               areasize = 6, conf.s.prefs = c(2,5,6,7), horizontalscenario = 1)
+#' elements <- list(elem1, elem2)
+#' scenarios <- lapply(elements, function(elem) {
+#'    lapply(c(0.2,0.5), function(x){
+#'       elem$threshold <- x
+#'       elem
+#'    })
+#' })
+#'
+#' xdata <- calculateScenarios(scenarios, nruns=2)
+calculateScenarios <- function(scenarios,nruns=10,nworkers=detectCores()) {
+
+  library(digest)
+  hash <- digest(scenarios)
+  filename <- paste('/tmp/',hash,'.rds',sep='')
+  if (file.exists(filename)) {
+    return(readRDS(filename))
+  }
+
+  equaldist <- function(x) {
+    runif(x)
+  }
+
+  category <- function(c) {
+    function(x) {
+      round(runif(x) * c + 0.5)
+    }
+  }
+
+  ######### Run ##############
+  applyresults <- lapply(scenarios, function(elements) {
+    rowresults <- mclapply(elements, function(elem) { # Loop over elements
+      occupancy <- elem$occupancyrate
+      nStudents <- elem$nStudents
+      nColleges <- elem$nColleges
+      threshold <- elem$threshold
+      areasize <- elem$areasize
+      j <- elem$horizontalscenario
+      s.prefs.count = elem$conf.s.prefs
+      quota <- elem$quota
+
+      mean <- (nStudents/nColleges)/occupancy # Mean number of places per program
+      sd <- mean/2 # Standard deviation for distribution of places per program
+      capacityfun <- function(n, mean, sd=1) {
+        sapply(round(rnorm(n, mean=mean, sd=sd)), function(x) max(1,x))
+      }
+      nSlots <- capacityfun(nColleges, mean, sd)
+
+      private <- function(x) {
+        runif(x) < quota
+      }
+
+      daresult <- stabsim3(m=nruns, nStudents=nStudents, nSlots=nSlots, verbose=FALSE,
+                           colleges = c("cx","cy", "firstpref", "secondpref", "quality", "cidiocat", "cidio1", "cidio2", "private"),
+                           students = c("sx", "sy", "social", "sidiocat", "idist", "iidio", "sidio1", "sidio2", "iquality"),
+                            colleges_fun = c(category(areasize),category(areasize),category(3),category(2),equaldist,category(10),equaldist,equaldist,private),
+                           students_fun = c(category(areasize),category(areasize),category(100),category(10),equaldist,equaldist,equaldist,equaldist,equaldist),
+                           outcome = ~ I(sqrt(((cx-sx)/areasize)**2 + ((cy-sy)/areasize)**2)),
+                           selection = c(
+                             student = ~ I((1000**(firstpref %% 3)) * ((private + j) < 2) * (abs(cx-sx)<=1) * (abs(cy-sy)<=1))
+                             + I((1000**((firstpref + secondpref) %% 3)) * social)
+                             + I((1000**((firstpref - secondpref) %% 3)) * private * ceiling((cidio1 + sidio1 %% 1) * 100))
+                             #+ I((1000**((firstpref - secondpref) %% 3)) * private * (cidiocat == sidiocat) )
+                             ,
+                             colleges = ~ I(-idist * 2 * sqrt(((cx-sx))**2 + ((cy-sy))**2)/areasize)
+                             + I(iquality * quality)
+                             + I(iidio * (cidiocat == sidiocat))
+                           ),
+                           private_college_quota = quota,
+                           count.waitinglist = function(x) {x}, s.prefs.count = s.prefs.count)
+
+      curr <- 0
+      for (m in 1:nruns) { # Average results
+        iteration <- daresult$iterations[[m]]
+        iterationtable <- t(as.matrix(iteration[,-1]))
+        complete <- sum(iterationtable[,1])
+        ratio <- complete * threshold
+        curr <- curr + sum(iteration$new+iteration$altered > ratio) + 1
+      }
+
+      result <- curr/nruns
+      # Clean workspace of heavy objects
+      rm(daresult)
+      gc()
+      return(result)
+
+    }, mc.silent=FALSE, mc.cores=nworkers)
+  })
+  saveRDS(applyresults, file = filename)
+  return(applyresults)
+}
---
+++ R/output.R
@@ -0,0 +1,82 @@
+#' Formats a double as percent
+#'
+#' @param x double value to be formated.
+#' @param digits integer number of digits after '.'.
+#'
+#' @return formatted string
+#' @export
+#'
+#' @examples
+#'
+#' percent(0.8)
+percent <- function(x, digits = 0) {
+  paste0(formatC(100 * x, format = 'f', digits = digits), "%")
+}
+
+
+#' Creates the plot for the scenario evaluation.
+#'
+#' This function generates a plot for the data returned by \code{\link{calculateScenarios}}.
+#' It plots a line diagram, where the x-Axis lists the different values of one dimension.
+#'
+#' @param data list of lists representing the data to be plotted
+#'     Each top level list represents one scenario.
+#'     This is the return of @method calculateScenarios().
+#' @param configuration list of lists representing the initial scenarios.
+#'     This should not include multiple entries based on the x-scale.
+#' @param dimensionx string the label of the x-axis.
+#' @param dimensionxvals vector representing the values of the x-axis.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' xdimensionvals = c(0.2,0.5)
+#' elem1 <- list(occupancyrate = .8, quota = .3, nStudents = 2700, nColleges = 600,
+#'               areasize = 7, conf.s.prefs = c(3,7,10,10), horizontalscenario = 1)
+#' elem2 <- list(occupancyrate = .8, quota = .3, nStudents = 600, nColleges = 200,
+#'               areasize = 6, conf.s.prefs = c(2,5,6,7), horizontalscenario = 1)
+#' elements <- list(elem1, elem2)
+#' scenarios <- lapply(elements, function(elem) {
+#'    lapply(xdimensionvals, function(x){
+#'       elem$threshold <- x
+#'       elem
+#'    })
+#' })
+#'
+#' xdata <- calculateScenarios(scenarios, nruns=2)
+#' plotEvaluation(xdata, elements, "Threshold", xdimensionvals)
+plotEvaluation <- function(data, configuration, dimensionx, dimensionxvals) {
+  relevantForLegend <- c("occupancyrate", "quota", "nStudents", "nColleges")
+  translationsForLegend <- list("occupancyrate"="Occupancy rate:", "quota" = "Private quota:", "nStudents" = "#Children:", "nColleges" = "#Programmes:  ")
+  translationsForResults <- list("occupancyrate"=identity, "quota" = percent, "nStudents" = identity, "nColleges" = identity, "threshold" = percent)
+
+  #Initialize Plot
+  par(mfrow=c(1, 1), mar=c(4, 4, 3, 3),xpd=FALSE)
+  plot(NULL, xlim=c(1,length(dimensionxval)),ylim = c(0,15), xaxt = 'n', yaxt = 'n', xlab = dimensionx, ylab = 'Iterations')
+  axis(side=1, at=c(1:length(dimensionxval)), labels = dimensionxlabels, col = NA, col.ticks = 1)
+  axis(side=2, at=(0:7)*2, labels = (0:7)*2, col = NA, col.ticks = 1)
+  abline(h=6,col='red')
+
+  colors = colors()[c(73,74,139,116,143, 50)]
+
+
+  for (i in c(1:length(data))) {
+    if (is.null(data[[i]])) {
+      #results[i,j,r,o] <- array()
+      print("Empty result occured")
+    } else {
+      lines(unlist(data[[i]]), pch = 1, type = "b", lty = i, col=colors[i])
+    }
+  }
+  legendentries <- lapply(configuration, function(elem) {
+    entries = c()
+    for (dim in relevantForLegend) {
+      entry <- paste0(c(translationsForLegend[[dim]], translationsForResults[[dim]](elem[[dim]])), collapse=" ")
+      entries <- cbind(entries, entry)
+    }
+    paste(entries, collapse=", ")
+  })
+  rowsrange <- 1:length(data)
+  legend('topright', legend = legendentries, col = colors[rowsrange], lty = rowsrange, pch = 1, cex=.8)
+}
