05c4861e17910d9f3356f50e2b041a9750ad7a99
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2020-01-15 15:32:00 +0100

    Remove duplicate elements

--- R/configurations.R
+++ R/configurations.R
@@ -317,15 +317,17 @@ quota_configuration <- function() {
               areasize = 7, conf.s.prefs = c(3,7,10,10), horizontalscenario = 1, threshold = .05)
   elem2 <- list(occupancyrate = .8, nStudents = 2700, nColleges = 60,
               areasize = 7, conf.s.prefs = c(3,7,10,10), horizontalscenario = 1, threshold = .05)
+  elem3 <- list(occupancyrate = 1.2, nStudents = 2700, nColleges = 60,
+                areasize = 7, conf.s.prefs = c(3,7,10,10), horizontalscenario = 1, threshold = .05)
+  elem4 <- list(occupancyrate = .8, nStudents = 2700, nColleges = 60,
+                areasize = 7, conf.s.prefs = c(3,7,10,10), horizontalscenario = 1, threshold = .05)
   elem5 <- list(occupancyrate = 1.2, nStudents = 600, nColleges = 200,
               areasize = 6, conf.s.prefs = c(2,5,6,7), horizontalscenario = 1, threshold = .05)
-  elem3 <- list(occupancyrate = 1.2, nStudents = 2700, nColleges = 60,
-              areasize = 7, conf.s.prefs = c(3,7,10,10), horizontalscenario = 1, threshold = .05)
-  elem4 <- list(occupancyrate = .8, nStudents = 2700, nColleges = 60,
-              areasize = 7, conf.s.prefs = c(3,7,10,10), horizontalscenario = 1, threshold = .05)
   elem6 <- list(occupancyrate = .8, nStudents = 600, nColleges = 200,
               areasize = 6, conf.s.prefs = c(2,5,6,7), horizontalscenario = 1, threshold = .05)
-  elements <<- list(elem1, elem2, elem3, elem4, elem5, elem6)
+  elements <<- list(elem1, elem2,
+                    #elem3, elem4,
+                    elem5, elem6)
   rows <<- lapply(elements, function(elem) {
         lapply(dimensionxval, function(x){
             elem[[dimensionx]] <- x
